8985241a3e041eaa658f4cecfbcc24c668aeb19c
Laurettta <120516649+Laurettta@users.noreply.github.com>
2023-09-14 23:36:14 +0100

    Add js

---
+++ index.js
@@ -0,0 +1,11 @@
+        //    Assignment
+
+// 1. what are the difference between var, let, and const?
+
+// (a) var declarations are globally scoped or function scoped.They are only accessible within the function in which they are defined.
+
+// (b) let are block scoped. They are only visible within the block (enclosed by curly braces) in which they are defined, such as within an if statement block.
+// Use let when you need a variable that can change its value
+
+// (c) const are block scoped. Variables declared with const are constants, meaning their value cannot be changed once its assigned
+// const is also hoisted but are not initialized.
