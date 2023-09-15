eb4594c06f9ca416653756023bc90900c75edf58
Laurettta <120516649+Laurettta@users.noreply.github.com>
2023-09-15 16:49:11 +0100

    Add js

--- index.js
+++ index.js
@@ -9,3 +9,19 @@
 
 // (c) const are block scoped. Variables declared with const are constants, meaning their value cannot be changed once its assigned
 // const is also hoisted but are not initialized.
+
+
+// 2. Javascript Data types
+
+// Boolean: Boolean value is one that can either be TRUE or FALSE. Booleans are often used in conditional testing. if you need to know "yes" or "no" about something,
+// then you would want to use the boolean function.
+
+// Undefined: Undefined is when a variable is declared and not initialized or not assigned with any value. By default, the variable was stored with an Undefined value.
+
+// null: null is a special value that represent an empty or unknown value. For instance, let number = null; 
+// The code above suggests that the number variable is empty at the moment and may have a value later.
+
+// Symbols: Symbols are immutable (cannot be changed) and are unique, which means that no two Symbols will ever be equal,
+// even if they have the same description. Also, it's often used as a property key in objects.
+
+// Objects: An Objects data type is a method for encoding key value pairs. Objects are created using curly braces '{}' and can contain zero or more key-value pairs. 
